d1f3a6df5f0c82c568e6c25823285b5bb1d028c4
Jiyu <agu0830@naver.com>
2023-02-04 20:19:39 +0900

    feat: add anchor

--- pybo/models.py
+++ pybo/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE) #User=회원가입시 데이터 저장에 사용했던 모델
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question') #User=회원가입시 데이터 저장에 사용했던 모델
     subject = models.CharField(max_length=200) #제목 최대 200자. 글자수 제한 텍스트 CharField
     content = models.TextField() #글자수 제한 없는 텍스트 TextField
     create_date = models.DateTimeField() #날짜, 시간 관련은 DateTimeField
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question') #추천인 추가
 
     def __str__(self): #id값 대신 제목으로 표시
        return self.subject
 
+
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE) #해당 답변과 연결된 질문이 삭제될경우 답변도 삭제됨.
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
