4774dbdea2890c1497594423b9409d779cc417c3
Jiyu <agu0830@naver.com>
2023-02-04 20:47:19 +0900

    feat: add markdown

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,8 +1,16 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.filter #템플릿 필터 함수 만들기
-def sub(value, arg): #기존값 value, 입력받은 값 arg
+def sub(value, arg): #기존값 value, 입력받은 값 ag
     return value - arg
+
+
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
